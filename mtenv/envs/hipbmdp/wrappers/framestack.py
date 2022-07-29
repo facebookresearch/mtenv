dc1efdb2f093b23256cd0afe861e67d268c783bd
Shagun Sodhani <sodhani@fb.com>
2022-07-29 13:48:54 -0700

    Fix lint errors

--- mtenv/envs/hipbmdp/wrappers/framestack.py
+++ mtenv/envs/hipbmdp/wrappers/framestack.py
@@ -2,6 +2,7 @@
 """Wrapper to stack observations for single task environments."""
 
 from collections import deque
+from typing import Any, Dict, Tuple
 
 import gym
 import numpy as np
@@ -23,24 +24,26 @@ class FrameStack(gym.Wrapper):  # type: ignore[type-arg]
         self._k = k
         self._frames: deque = deque([], maxlen=k)
         shp = env.observation_space.shape
+        obs_space_dtype = env.observation_space.dtype
+        assert obs_space_dtype is not None
         self.observation_space = gym.spaces.Box(
             low=0,
             high=1,
-            shape=((shp[0] * k,) + shp[1:]),
-            dtype=env.observation_space.dtype,
+            shape=((shp[0] * k,) + shp[1:]),  # type: ignore[index]
+            dtype=obs_space_dtype,  # type: ignore[arg-type]
         )
-        self._max_episode_steps = env._max_episode_steps
+        self._max_episode_steps = env._max_episode_steps  # type: ignore[attr-defined]
 
-    def reset(self) -> np.ndarray:
-        obs = self.env.reset()
+    def reset(self, **kwargs: Dict[str, Any]) -> np.ndarray:  # type: ignore[override]
+        obs = self.env.reset(**kwargs)  # type: ignore[arg-type]
         for _ in range(self._k):
             self._frames.append(obs)
         return self._get_obs()
 
     def step(self, action: ActionType) -> EnvStepReturnType:
-        obs, reward, done, info = self.env.step(action)
+        obs, *reward_done_info = self.env.step(action)
         self._frames.append(obs)
-        return self._get_obs(), reward, done, info
+        return self._get_obs(), *reward_done_info  # type: ignore[return-value]
 
     def _get_obs(self) -> np.ndarray:
         assert len(self._frames) == self._k
